721293e886d76e35e56820fc5565a148cd840186
muraisi0330 <137864654+muraisi0330@users.noreply.github.com>
2026-01-20 13:26:59 +0900

    a

---
+++ Assets/Scripts/StageChangeScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using TMPro;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class StageChangeScript : MonoBehaviour
+{
+    [SerializeField] 
+     TextMeshProUGUI stageText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        
+    }
+    public void StageUpdate(string stageName)
+    {
+        if (stageText != null)
+        {
+            stageText.text = stageName;
+        }
+        this.gameObject.SetActive(true);
+    }
+}
