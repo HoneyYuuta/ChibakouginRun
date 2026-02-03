3beac77be0122016d2f43e4b0c017eb95b467269
muraisi0330 <137864654+muraisi0330@users.noreply.github.com>
2026-02-03 12:49:31 +0900

    a

---
+++ Assets/Scripts/Settings UI/SettingsUIProcessing.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsUIProcessing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject UI;
+    [SerializeField]
+    GameObject settingUI;
+    [SerializeField]
+    GameObject volumeUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void WhenBackIsPressed()
+    {
+        UI.SetActive(true);
+        settingUI.SetActive(false);
+        volumeUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+    //設定が押されたとき
+    public void WhenSettingsIsPressed()
+    {
+        UI.SetActive(false);
+        settingUI.SetActive(true);
+        volumeUI.SetActive(false);
+
+        Time.timeScale = 0;
+    }
+    //音量が押されたとき
+    public void WhenVolumeIsPressed()
+    {
+        UI.SetActive(false);
+        settingUI.SetActive(false);
+        volumeUI.SetActive(true);
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Start UI/StartUI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class StartUI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject startUI;
+    //テキスト
+    [SerializeField]
+    TextMeshProUGUI startText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCountDown();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //カウントダウン開始
+    public void StartCountDown()
+    {
+        startUI.SetActive(true);
+        var fadeIn = DOTween.Sequence();
+        //３秒カウントダウン
+        fadeIn.Append(startText.DOFade(0, 1f).SetDelay(1f))
+            .AppendCallback(() =>
+            {
+                startText.DOFade(1, 0);
+                startText.text = "2";
+            }).
+            Append(startText.DOFade(0, 1f).SetDelay(1f))
+            .AppendCallback(() =>
+            {
+                startText.DOFade(1, 0);
+                startText.text = "1";
+            }).
+            Append(startText.DOFade(0, 1f).SetDelay(1f))
+            .AppendCallback(() =>
+            {
+                startText.DOFade(1, 0);
+                startText.text = "Start!";
+            }).
+            Append(startText.DOFade(0, 1f).SetDelay(1f))
+            .AppendCallback(() =>
+            {
+                startUI.SetActive(false);
+            });
+
+
+    }
+}
