96281e4ae034dfe0c93c85e92dc8fc0e77508ea4
muraisi0330 <137864654+muraisi0330@users.noreply.github.com>
2026-02-06 12:15:56 +0900

    a

---
+++ Assets/Scripts/UI/RankingScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RankingScript : MonoBehaviour
+{
+    //ランキングテキスト
+    [SerializeField]
+    TextMeshProUGUI[] rankText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayRanking();
+    }
+    //ランキング表示
+    public void DisplayRanking()
+    {
+        for (int i = 0; i < rankText.Length; i++)
+        {
+            rankText[i].text = SaveData.SetRankingName(i) + " " + "点";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
